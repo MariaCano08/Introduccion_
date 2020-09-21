d58f1a9c24b883fbde5fc4b59e7a30a2fdca7930
María Cano <maria.cano4435@alumnos.udg.mx>
2020-09-20 23:26:00 -0500

    Capturamos 2 numero y hacemos 4 las operaciones basicas con esos numeros

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ func main() {
 	fmt.Println("Hola mundo\nDame un numero")
 	fmt.Scan(&a)
 	fmt.Println("Dame otro numero")
+	fmt.Scan(&b)
 	c = a + b
 	fmt.Println("La suma de:", a, "+", b, " es:", c)
+	c = a - b
+	fmt.Println("La resta de:", a, "-", b, " es:", c)
+	c = a * b
+	fmt.Println("La multiplicacion de:", a, "*", b, " es:", c)
+	c = a / b
+	fmt.Println("La Division de:", a, "/", b, " es:", c)
 }
