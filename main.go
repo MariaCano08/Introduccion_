081062c89fd09f79b89c7af59c20806e73856e53
María Cano <maria.cano4435@alumnos.udg.mx>
2020-09-28 03:37:49 -0500

    Agregamos Hola Corazon

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	var a, b, c float64
-	fmt.Println("Hola mundo\nDame un numero")
+	fmt.Println("Hola corazon")
+	fmt.Println("Dame un numero")
 	fmt.Scan(&a)
 	fmt.Println("Dame otro numero")
 	fmt.Scan(&b)
